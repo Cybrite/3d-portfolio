1602119e73e9728a74901b33b516a76e806bfc49
Harsh <harshtanishq2002@gmail.com>
2024-10-26 16:18:47 +0530

    feat: added navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,49 @@
+import {useState} from "react";
+import {navLinks} from "../content/index.js";
+
+const NavItems = () => {
+    return (
+        <ul className='nav-ul'>
+            {navLinks.map(({id , href , name}) => (
+                <li key={id} className='nav-li'>
+                  <a href={href} className='nav-li_a' onClick={() => {}}>
+                     {name}
+                  </a>
+                </li>
+            ))}
+        </ul>
+    )
+};
+
 const Navbar = () => {
+    const[open, setOpen] = useState(false)
+
+    function handleClick() {
+        setOpen(!open)
+    }
+
     return (
         <header className='fixed top-0 left-0 right-0 z-100 bg-black/90'>
-            <div className='max-w-7xl mx-auto '>
-                <div className='flex justify-center items-center py-5 mx-auto c-space'>
-                    <a href='/' className='text-neutral-400 font-bold text-xl hover:text-white transition-colors'>
+            <div className='mx-auto max-w-7xl '>
+                <div className='flex items-center justify-between py-5 mx-auto c-space'>
+                    <a href='/' className='text-xl font-bold transition-colors text-neutral-400 hover:text-white'>
                         Harsh
                     </a>
 
-                    <button>
-                        <img src={} />
+                    <button onClick={() => handleClick()} className='text-neutral-400 hover:text-white focus:outline-none sm:hidden flex aria-label="toggle menu" '>
+                        <img src={open ? 'assets/close.svg' : 'assets/menu.svg'} alt='toggle' className='w-6 h-6' />
                     </button>
+
+                    <nav className='hidden sm:flex'>
+                        <NavItems/>
+                    </nav>
                 </div>
+            </div>
+
+            <div className={`nav-sidebar ${open ? 'max-h-screen' : 'max-h-0'}`}>
+                <nav className='p-5'>
+                    <NavItems/>
+                </nav>
             </div>
         </header>
 
