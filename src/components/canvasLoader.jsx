359294028f3e30d9df48af3822fd11b7e30c3491
Harsh <harshtanishq2002@gmail.com>
2024-10-31 07:09:32 +0530

    feat: added new comp model and loader

--- src/components/canvasLoader.jsx
+++ src/components/canvasLoader.jsx
@@ -1,4 +1,5 @@
-import { Html, useProgress } from '@react-three/drei';
+import { Html, useProgress } from "@react-three/drei";
+import "ldrs/hatch";
 
 const CanvasLoader = () => {
   const { progress } = useProgress();
@@ -7,20 +8,24 @@ const CanvasLoader = () => {
       as="div"
       center
       style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'column',
-      }}>
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
       <span className="canvas-loader"></span>
+
+      <l-hatch size="28" stroke="4" speed="3.5" color="white"></l-hatch>
       <p
         style={{
           fontSize: 14,
-          color: '#F1F1F1',
+          color: "#F1F1F1",
           fontWeight: 800,
           marginTop: 40,
-        }}>
-        {progress !== 0 ? `${progress.toFixed(2)}%` : 'Loading...'}
+        }}
+      >
+        {progress !== 0 ? `${progress.toFixed(2)}%` : "Loading..."}
       </p>
     </Html>
   );
